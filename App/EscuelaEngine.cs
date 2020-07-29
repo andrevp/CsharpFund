3ff24d7c13271801177acc27998fab74816b40b4
Carlos Andrés <cvcarlosandres39@gmail.com>
2020-07-29 16:13:07 -0500

    Prueba de conexion SSH

--- App/EscuelaEngine.cs
+++ App/EscuelaEngine.cs
@@ -96,7 +96,7 @@ namespace CoreEscuela
         {
             return GetObjetosEscuela(out conteoEvaluaciones, out conteoCursos, out conteoAsignaturas, out int dummy);
         }
-        // Método original para obtener objetos. Este es un cambio del nuevo Branch de VersionPruebas
+        // Método original para obtener objetos.
         public IReadOnlyList<ObjetoEscuelaBase> GetObjetosEscuela(
             out int conteoEvaluaciones,
             out int conteoCursos,
